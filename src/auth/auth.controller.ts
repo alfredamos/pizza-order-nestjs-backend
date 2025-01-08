51ad0f2214f6e797e833bf14b716ab1756bb2c7f
Alfred Akintunde AMOS <52054578+alfredamos@users.noreply.github.com>
2025-01-07 20:31:50 -0500

    auth functionalities are guarded as per assigned roles.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -11,26 +11,34 @@ import { AuthService } from './auth.service';
 import { ChangePasswordDto } from './dto/change-password.Dto';
 import { EditProfileDto } from './dto/edit-profile.Dto';
 import { LoginDto } from './dto/loginDto';
+import { UserInfo } from 'src/models/userInfo.model';
+import { CurrentUser } from 'src/decorators/current-user.decorator';
+import { Roles } from 'src/decorators/role.decorator';
+import { IsPublic } from 'src/decorators/is-public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Patch()
+  @Roles('Admin', 'Staff', 'User')
+  @Patch('change-password')
   async changePassword(@Body() changePasswordDto: ChangePasswordDto) {
     return await this.authService.changePassword(changePasswordDto);
   }
 
-  @Get(':id')
-  async currentUser(@Param('id') id: string) {
-    return await this.authService.currentUser(id);
+  @Roles('Admin', 'Staff', 'User')
+  @Get('current-user')
+  async currentUser(@CurrentUser() user: UserInfo) {
+    return await this.authService.currentUser(user?.id);
   }
 
-  @Patch()
+  @Roles('Admin', 'Staff', 'User')
+  @Patch('edit-profile')
   async editProfile(@Body() editProfileDto: EditProfileDto) {
     return await this.authService.editProfile(editProfileDto);
   }
 
+  @IsPublic()
   @Post()
   async login(@Body() loginDto: LoginDto) {
     return await this.authService.login(loginDto);

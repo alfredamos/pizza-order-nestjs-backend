5f83afb1e9fd7322fb1eb30e81470d54c9783d9c
Alfred Akintunde AMOS <52054578+alfredamos@users.noreply.github.com>
2025-01-08 13:05:46 -0500

    add access by role  to order-controller

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -11,31 +11,37 @@ import {
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { Roles } from 'src/decorators/role.decorator';
 
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
+  @Roles('Admin', 'Staff', 'User')
   @Post()
   createOrder(@Body() createOrderDto: UpdateOrderDto) {
     return this.ordersService.createOrder(createOrderDto);
   }
 
+  @Roles('Admin', 'Staff', 'User')
   @Post()
   async orderCreate(@Body() createOrderDto: CreateOrderDto) {
     return await this.ordersService.orderCreate(createOrderDto);
   }
 
+  @Roles('Admin', 'Staff', 'User')
   @Get()
   async findAll() {
     return await this.ordersService.getAllOrders();
   }
 
+  @Roles('Admin', 'Staff', 'User')
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return await this.ordersService.getOneOrder(id);
   }
 
+  @Roles('Admin', 'Staff', 'User')
   @Patch(':id')
   async update(
     @Param('id') id: string,
@@ -44,26 +50,31 @@ export class OrdersController {
     return await this.ordersService.editOrder(id, updateOrderDto);
   }
 
+  @Roles('Admin', 'Staff', 'User')
   @Delete(':id')
   async remove(@Param('id') id: string) {
     return await this.ordersService.deleteOrderById(id);
   }
 
+  @Roles('Admin', 'Staff', 'User')
   @Get('orders-by-user-id/:userId')
   async findAllByUserId(@Param('userId') userId: string) {
     return await this.ordersService.getAllOrdersByUserId(userId);
   }
 
+  @Roles('Admin')
   @Delete('delete-all-orders-by-user-id/:userId')
   async deleteOrdersByUserId(@Param('userId') userId: string) {
     return await this.ordersService.deleteOrdersByUserId(userId);
   }
 
+  @Roles('Admin')
   @Patch('delivered/:orderId')
   async deliveredOrder(@Param('orderId') orderId: string) {
     return await this.ordersService.orderDelivered(orderId);
   }
 
+  @Roles('Admin')
   @Patch('shipped/:orderId')
   async shippedOrder(@Param('orderId') orderId: string) {
     return await this.ordersService.orderShipped(orderId);
